3c126e13f4b6f1308f35a8382ee8fcd035d767e6
Aleksey Makarov <leha199600@gmail.com>
2022-07-30 15:53:49 +0300

    Homework v0.1

--- app/src/main/java/com/example/otusalekseymakarovmovies/MovieDetails.kt
+++ app/src/main/java/com/example/otusalekseymakarovmovies/MovieDetails.kt
@@ -2,7 +2,10 @@ package com.example.otusalekseymakarovmovies
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -19,15 +22,21 @@ class MovieDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
         val title = intent.getStringExtra("Title")
         val imageUrl = intent.getStringExtra("ImageUrl")
         Log.i("HELLL", title!!)
-        val backgroundImage = findViewById<ImageView>(R.id.movie_details_background)
-        val ctx = this
-        backgroundImage.load(imageUrl){
+        findViewById<ImageView>(R.id.movie_details_background)
+            .load(imageUrl){
             allowHardware(false)
-            transformations(BlurTransformation(ctx, 25f))
-
+            transformations(BlurTransformation(this@MovieDetails, 25f))
+        }
+        findViewById<TextView>(R.id.textViewMovieAgeRestrictions).text = intent.getIntExtra("AgeRestriction", 0).let { "+$it" }
+        findViewById<TextView>(R.id.textViewFilmName).text = intent.getStringExtra("Title")
+        findViewById<TextView>(R.id.textViewDescription).text=intent.getStringExtra("Description")
+        findViewById<RatingBar>(R.id.ratingBarMovieRatingDetails).rating = intent.getIntExtra("AgeRestriction", 0).toFloat()
+        findViewById<ImageView>(R.id.movie_image).load(imageUrl){
+            allowHardware(false)
         }
 
     }
